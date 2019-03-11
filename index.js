a02104b013f6338fd9a393008bda2e9c5b47a0b2
nitpum <nitpum@outlook.com>
2019-03-11 23:06:54 +0700

    Updating index.js

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-/**
-* @param context {WebtaskContext}
-*/
-module.exports = function(context, cb) {
-  cb(null, { hello: context.query.name || 'Anonymous' });
-};
+const qrcode = require('qrcode');
+const promptpay = require('promptpay-qr');
+const axios = require('axios');
+
+module.exports = function (context) {
+  var user = context.secrets.users;
+  var amount = context.secrets.amount;
+
+  const payload = generatePayload(context.secrets.promptpayId, { amount });
+  var options = { type: 'png', color: { dark: '#003b6a', light: '#f7f8f7' } }
+  new Promise((resolve, reject) => {
+    qrcode.toString(payload, options, (err, svg) => {
+      if (err) return reject(err)
+      resolve(svg)
+    })
+  })
+
+  user.forEach(element => {
+    axios.get('https://discordapp.com/api/v6/users/@me/channels', {
+      headers: {
+        'Authorization': 'Bot ' + context.secrets.botId,
+        'Content-Type': 'application/json',
+        'recipient_id': element
+      }
+    })
+      .then(function (res) {
+        console.log(res);
+      });
+    
+  });
+}
