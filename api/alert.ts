a6d38a7c053bb470b3784d3ff8b610c93e73b8d6
nitpum <nitpum@outlook.com>
2020-06-20 16:15:59 +0700

    chore: Setup pre-hook commit

--- api/alert.ts
+++ api/alert.ts
@@ -1,51 +1,51 @@
-import { NowRequest, NowResponse } from '@now/node'
-import axios from 'axios'
+import { NowRequest, NowResponse } from "@now/node";
+import axios from "axios";
 // import qeneratePayload from 'promptpay-qr'
 // import qrcode from '../modules/qrcode'
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const botToken = process.env.DISCORD_BOT_TOKEN
-  const users = process.env.DISCORD_USERS.split(',')
-  const amount = process.env.NETFLIX_AMOUNT
-  const promptpayId = process.env.PROMPTPAY_ID
-  const promptpayQr = process.env.PROMPTPAY_QR
+  const botToken = process.env.DISCORD_BOT_TOKEN;
+  const users = process.env.DISCORD_USERS.split(",");
+  const amount = process.env.NETFLIX_AMOUNT;
+  const promptpayId = process.env.PROMPTPAY_ID;
+  const promptpayQr = process.env.PROMPTPAY_QR;
 
   // const payload = qeneratePayload(promptpayId, { amount })
   // const svg = await qrcode(payload)
 
-  users.forEach(async (userId) => {
+  users.forEach(async (userId: String) => {
     // Get user DM Id
     const res = await axios.post(
-      'https://discordapp.com/api/v6/users/@me/channels',
+      "https://discordapp.com/api/v6/users/@me/channels",
       { recipient_id: userId.trim() },
       {
         headers: {
-          Authorization: 'Bot ' + botToken,
-          'Content-Type': 'application/json',
+          Authorization: "Bot " + botToken,
+          "Content-Type": "application/json",
         },
       }
-    )
+    );
 
     if (res.data && res.data.id) {
       axios.post(
-        'https://discordapp.com/api/v6/channels/' + res.data.id + '/messages',
+        "https://discordapp.com/api/v6/channels/" + res.data.id + "/messages",
         {
-          content: 'Netflix monthly 100 Baht.',
+          content: "Netflix monthly 100 Baht.",
           embed: {
-            title: 'Netflix',
-            url: 'https://discordapp.com',
+            title: "Netflix",
+            url: "https://discordapp.com",
             color: 9895936,
             image: {
               url: promptpayQr,
             },
             fields: [
               {
-                name: 'Promptpay',
+                name: "Promptpay",
                 value: promptpayId,
                 inline: true,
               },
               {
-                name: 'Value (Baht)',
+                name: "Value (Baht)",
                 value: amount,
                 inline: true,
               },
@@ -54,14 +54,14 @@ export default async (req: NowRequest, res: NowResponse) => {
         },
         {
           headers: {
-            Authorization: 'Bot ' + botToken,
-            'Content-Type': 'application/json',
+            Authorization: "Bot " + botToken,
+            "Content-Type": "application/json",
           },
         }
-      )
+      );
     }
-  })
+  });
 
-  res.status(200)
-  res.send('')
-}
+  res.status(200);
+  res.send("");
+};
